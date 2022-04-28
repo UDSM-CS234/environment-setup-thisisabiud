c4b645b283eda8637675dbf6b69e8041452e59f8
samo abihud thomas <iam.abiud@yandex.com>
2022-04-28 09:49:08 +0300

    2020-04-11046 lab 1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,6 +1,6 @@
 package com.company;
 
-public class Main {
+public class Main{
 
     public static void main(String[] args) {
         double gravity = -9.81; // Earth's gravity in m/s^2
@@ -10,5 +10,9 @@ public class Main {
         double finalPosition = 0.0;
         System.out.println("The object's position after " + fallingTime +
                 " seconds is " + finalPosition + " m.");
+
+        double answer = 0.5 * gravity * fallingTime * fallingTime + initialVelocity * fallingTime + initialPosition;
+
+        System.out.println("The answer is " + answer + "m.");
     }
 }
